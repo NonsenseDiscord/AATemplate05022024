89b3b7e798bb693ee902cbded3a29227ce62f3c7
BenScriptsAA <168475069+BenScriptsAA@users.noreply.github.com>
2024-05-03 15:03:27 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-message = "Your ad:\n" #Make sure you do \n for enter, if not it will not work
+message = "Welcome to Shadow Realm.\nA Friendly Server For Making Friends And Playing Games\n\nWhat we offer you:\nMovie Nights \nFun Games\nCountryball.\nActive and friendly community\nA variety of text channels\nAllot of voice channels\nStaff applications.\nCustom Emoji's\n\nLooking for\nNew Members\nServer Boosts.\n\nJoin to become a member.\nhttps://discord.gg/Hdw4PPze" #Make sure you do \n for enter, if not it will not work
 
 
 channels = {"00000000000000000000"}
@@ -6,5 +6,5 @@ channels = {"00000000000000000000"}
 
 MPS = 60 # By default 60 seconds per message, lower this as much as you want (60 = Hard to detect, 45 = Detectable but unlikely, 30 = You have a ton of channels, less = detectable (May get banned off Discord)
 
-sessionId = '000000000' #Your sessionId
+sessionId = 'nhm6qfzjq' #Your sessionId
 
