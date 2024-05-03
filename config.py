afd896a1986363659d56e0bcceed8a872b2f8857
BenScriptsAA <168475069+BenScriptsAA@users.noreply.github.com>
2024-05-03 14:32:27 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 message = "Your ad:\n" #Make sure you do \n for enter, if not it will not work
 
 
-channels = {"0000000000000000000"}
+channels = {"1236067799776891003"}
 
 
 MPS = 60 # By default 60 seconds per message, lower this as much as you want (60 = Hard to detect, 45 = Detectable but unlikely, 30 = You have a ton of channels, less = detectable (May get banned off Discord)
 
-sessionId = '000000000' #Your sessionId
+sessionId = 'nhm6qfzjq' #Your sessionId
 
