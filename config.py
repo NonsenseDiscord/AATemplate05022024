fd532d39efc34a06c3558ae82a2969cb036d86ad
NonsenseDiscord <keanovanzoolingen@icloud.com>
2024-05-04 20:58:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 message = "Welcome to Shadow Realm.\nA Friendly Server For Making Friends And Playing Games\n\nWhat we offer you:\nMovie Nights \nFun Games\nCountryball.\nActive and friendly community\nA variety of text channels\nAllot of voice channels\nStaff applications.\nCustom Emoji's\n\nLooking for\nNew Members\nServer Boosts.\n\nJoin to become a member.\nhttps://discord.gg/Hdw4PPze" #Make sure you do \n for enter, if not it will not work
 
 
-channels = {"1236067799776891003"}
+channels = {"997916982080983040", "1046107293332283483", "757476260690657281", "812665054150393866", "549952532693385226", "549951891522453542", "996649784318365736", "997932800843989022", "772126569334046724", "583558674711052300", "583557943681875988", "1007924026024398932", "1007924026196377724", "1007924026024398936", "878922454154772551", "1035885748479594586", "1121061718546726912"
             
 
-MPS = 60 # By default 60 seconds per message, lower this as much as you want (60 = Hard to detect, 45 = Detectable but unlikely, 30 = You have a ton of channels, less = detectable (May get banned off Discord)
+MPS = 500 # By default 60 seconds per message, lower this as much as you want (60 = Hard to detect, 45 = Detectable but unlikely, 30 = You have a ton of channels, less = detectable (May get banned off Discord)
 
 sessionId = 'nhm6qfzjq' #Your sessionId
 
